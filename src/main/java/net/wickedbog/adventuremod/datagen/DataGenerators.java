19320476243d586b6c88f62320fcb21071fdfd6c
Liam5095 <gamerliam12@outlook.com>
2024-11-10 08:19:38 +0100

    Advancements

--- src/main/java/net/wickedbog/adventuremod/datagen/DataGenerators.java
+++ src/main/java/net/wickedbog/adventuremod/datagen/DataGenerators.java
@@ -7,6 +7,7 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
@@ -37,5 +38,6 @@ public class DataGenerators {
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
 
         generator.addProvider(event.includeClient(), new ModWorldGenProvider(packOutput, lookupProvider));
+        generator.addProvider(event.includeClient(), new AdvancementProvider(packOutput, lookupProvider, existingFileHelper, List.of(new ModAdvancementProvider())));
     }
 }
---
+++ src/main/java/net/wickedbog/adventuremod/datagen/ModAdvancementProvider.java
@@ -0,0 +1,32 @@
+package net.wickedbog.adventuremod.datagen;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementType;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.data.AdvancementProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.wickedbog.adventuremod.AdventureMod;
+import net.wickedbog.adventuremod.block.ModBlocks;
+import net.wickedbog.adventuremod.item.ModItems;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class ModAdvancementProvider implements AdvancementProvider.AdvancementGenerator {
+    @Override
+    public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper existingFileHelper) {
+        Advancement rootAdvancement = Advancement.Builder.advancement()
+                .display(new DisplayInfo(new ItemStack(ModBlocks.HEARTWOOD_LOG.get()),
+                        Component.literal("Adventure Mod"), Component.literal("This a weird tree..."),
+                        Optional.of(ResourceLocation.fromNamespaceAndPath(AdventureMod.MOD_ID, "textures/block/heartwood_log.png")), AdvancementType.TASK,
+                        true, true, false))
+                .addCriterion("has_heartwood_log", InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.HEARTWOOD_LOG.get()))
+                .save(saver, ResourceLocation.fromNamespaceAndPath(AdventureMod.MOD_ID, "adventuremod"), existingFileHelper).value();
+    }
+}
